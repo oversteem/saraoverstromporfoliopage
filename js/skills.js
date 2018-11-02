0d30d70d536a8e7756793105e909694ca441447e
Sara Överström <saraoverstrom87@gmail.com>
2018-11-02 16:53:09 +0100

    cleaned up my code

--- js/skills.js
+++ js/skills.js
@@ -7,8 +7,7 @@ new Chart(document.getElementById("doughnut-chart1"), {
 				label: "Population (millions)",
 				backgroundColor: ["rgba(204, 50, 4, 0.774)", "rgba(13, 65, 177, 0.9)","rgba(238, 220, 59, 0.918)", "rgba(231, 56, 50, 0.685)"],
 				data: [25,25,25,25]
-				/*canvas.style.width  = "200px";
-				canvas.style.height = "125px"; */
+				
 				
 			}
 		]
@@ -30,8 +29,7 @@ new Chart(document.getElementById("doughnut-chart2"), {
 				label: "Population (millions)",
 				backgroundColor: ["rgba(204, 50, 4, 0.774)", "rgba(13, 65, 177, 0.9)","rgba(238, 220, 59, 0.918)", "rgba(231, 56, 50, 0.685)"],
 				data: [20,25,35, 20]
-				/*canvas.style.width  = "200px";
-				canvas.style.height = "125px"; */ 
+			
 			}
 		]
     },
